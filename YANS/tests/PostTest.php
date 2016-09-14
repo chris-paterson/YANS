a96d2550411491790c7569a87fd70801923100a0
chris-paterson <cjpaterson.3@gmail.com>
2016-09-14 12:11:41 +0100

    Now reset database at start to get clean DB.

--- YANS/tests/PostTest.php
+++ YANS/tests/PostTest.php
@@ -8,14 +8,16 @@ use App\User;
 
 class PostTest extends TestCase
 {
-    public function setUp(){
+    public function setUp() {
         parent::setUp();
 
         $this->prepareForTests();
+
     }
 
-    public function prepareForTests(){
+    public function prepareForTests(){ 
         Config::set('database.default', 'mysql_testing');
+        Artisan::call('migrate:reset');
         Artisan::call('migrate');
 
         $this->createUser();
@@ -36,16 +38,16 @@ class PostTest extends TestCase
      */
     public function testUserCanCreatePost()
     {
-        $user = App\User::find(1);
+        $user = App\User::all()->first();
         $this->be($user);
 
-        // $this->visitRoute('posts.create')
-        //     ->type('This is a test title', 'postTitle')
-        //     ->type('## Here is a markdown heading', 'postBody')
-        //     ->type('preview', 'preview')
-        //     ->check('publish')
-        //     ->type('3.00', 'price')
-        //     ->press('Submit')
-        //     ->seePageIs('posts.show');
+        $this->visitRoute('posts.create')
+            ->type('This is a test title', 'postTitle')
+            ->type('## Here is a markdown heading', 'postBody')
+            ->type('preview', 'preview')
+            ->check('publish')
+            ->type('3.00', 'price')
+            ->press('Submit')
+            ->seePageIs('posts.show');
     }
 }
